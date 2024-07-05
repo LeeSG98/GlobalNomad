ca535ded1f404cc3e17da220935f00e7033fd463
LeeSoonGu <rntnsl9280@naver.com>
2024-07-05 11:44:42 +0900

    feat: 로그인, 회원가입 양식

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginForm from "@/components/signin/LoginForm";
+
+const LoginPage = () => (
+  <div>
+    로그인을 진행해주세요
+    <LoginForm />
+  </div>
+);
+
+export default LoginPage;
