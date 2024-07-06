963ad8b1c1c2ce1908e838a856b823d349f55cfd
LeeSoonGu <rntnsl9280@naver.com>
2024-07-06 19:44:17 +0900

    Feat: 회원가입 유효성 검사 추가

--- src/components/signup/SignupInput.tsx
+++ src/components/signup/SignupInput.tsx
@@ -1,11 +1,15 @@
 import { ChangeEvent, useState } from "react";
 import Label from "../common/Label";
+import { SignupErrorType } from "@/types/SignupPage";
 
 interface SignupInputProps {
   inputName: string;
   onChangeInput: (e: ChangeEvent<HTMLInputElement>) => void;
   value: string;
   labelName: string;
+  inputType: string;
+  signupErrorMessage: SignupErrorType | null;
+  setSignupErrorMessage: React.Dispatch<React.SetStateAction<SignupErrorType>>;
 }
 
 const SignupInput = ({
@@ -13,47 +17,116 @@ const SignupInput = ({
   onChangeInput,
   value,
   labelName,
+  inputType,
+  signupErrorMessage,
+  setSignupErrorMessage,
 }: SignupInputProps) => {
   const [isShowInputValue, setIsShowInputValue] = useState(false);
-  const [inputType, setInputType] = useState(inputName);
+  const [changeInputType, setChangeInputType] = useState(inputType);
 
-  const onClickEyeIcon = () => {
-    if (inputType === "password") {
-      setInputType("text");
-    } else if (inputType === "text") {
-      setInputType("password");
+  const onClickInput = () => {
+    if (inputName === "email") {
+      setSignupErrorMessage((prev) => ({
+        ...prev,
+        emailErrorMessage: null,
+      }));
+    } else if (inputName === "nickname") {
+      setSignupErrorMessage((prev) => ({
+        ...prev,
+        nicknameErrorMessage: null,
+      }));
+    } else if (inputName === "password") {
+      setSignupErrorMessage((prev) => ({
+        ...prev,
+        passwordErrorMessage: null,
+      }));
+    } else if (inputName === "passwordConfirm") {
+      setSignupErrorMessage((prev) => ({
+        ...prev,
+        passwordConfirmErrorMessage: null,
+      }));
     }
+  };
+  const onClickEyeIcon = () => {
+    setChangeInputType((prevType) =>
+      prevType === "password" ? "text" : "password"
+    );
     setIsShowInputValue((prev) => !prev);
   };
+
+  let borderColorClass = "";
+  if (inputName === "email" && signupErrorMessage?.emailErrorMessage) {
+    borderColorClass = "border-red-F4";
+  } else if (
+    inputName === "nickname" &&
+    signupErrorMessage?.nicknameErrorMessage
+  ) {
+    borderColorClass = "border-red-F4";
+  } else if (
+    inputName === "password" &&
+    signupErrorMessage?.passwordErrorMessage
+  ) {
+    borderColorClass = "border-red-F4";
+  } else if (
+    inputName === "passwordConfirm" &&
+    signupErrorMessage?.passwordConfirmErrorMessage
+  ) {
+    borderColorClass = "border-red-F4";
+  }
 
   return (
     <div className="flex flex-col gap-2 relative">
       <Label labelName={labelName} />
-      <input
-        name={inputName}
-        type={inputType}
-        id={inputName}
-        onChange={onChangeInput}
-        value={value}
-        className="border border-gray-A4 rounded-[6px] px-5 py-4 focus:outline-none"
-      />
-      {labelName === "비밀번호" ? (
-        <button
-          onClick={onClickEyeIcon}
-          type="button"
-          className="text-[0px] absolute bottom-[16px] right-[20px]"
-        >
-          <img
-            src={
-              isShowInputValue === true
-                ? "/image/visibility_on.svg"
-                : "/image/visibility_off.svg"
-            }
-            alt="arrow_image"
-            className="w-[24px] h-[24px]"
-          />
-        </button>
-      ) : null}
+      <div className="relative">
+        <input
+          name={inputName}
+          type={changeInputType}
+          id={inputName}
+          onChange={onChangeInput}
+          value={value}
+          className={`border border-gray-A4 rounded-[6px] px-5 py-4 focus:outline-none w-full ${borderColorClass}`}
+          onClick={onClickInput}
+        />
+        {labelName.includes("비밀번호") === true ? (
+          <button
+            onClick={onClickEyeIcon}
+            type="button"
+            className="text-[0px] absolute bottom-[16px] right-[20px]"
+          >
+            <img
+              src={
+                isShowInputValue === true
+                  ? "/image/visibility_on.svg"
+                  : "/image/visibility_off.svg"
+              }
+              alt={isShowInputValue === true ? "open_eye" : "close_eye"}
+              className="w-[24px] h-[24px]"
+            />
+          </button>
+        ) : null}
+      </div>
+      {inputName === "email" && signupErrorMessage?.emailErrorMessage && (
+        <div className="text-red-F4 text-xs ml-1">
+          {signupErrorMessage.emailErrorMessage}
+        </div>
+      )}
+      {inputName === "nickname" && signupErrorMessage?.nicknameErrorMessage && (
+        <div className="text-red-F4 text-xs ml-1">
+          {signupErrorMessage.nicknameErrorMessage}
+        </div>
+      )}
+
+      {inputName === "password" && signupErrorMessage?.passwordErrorMessage && (
+        <div className="text-red-F4 text-xs ml-1">
+          {signupErrorMessage.passwordErrorMessage}
+        </div>
+      )}
+      {inputName === "passwordConfirm" &&
+        signupErrorMessage?.passwordConfirmErrorMessage && (
+          <div className="text-red-F4 text-xs ml-1">
+            {signupErrorMessage.passwordConfirmErrorMessage}
+          </div>
+        )}
     </div>
   );
 };
