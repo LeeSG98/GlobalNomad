812173261602753deb226e8462911d183621fc4e
ChoTD <127819128+ChoTD@users.noreply.github.com>
2024-07-06 16:29:08 +0900

    feat: 예약내역페이지 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div>GlobalNomad</div>
+      <div className="flex flex-col items-center gap-3 pt-5">
+        <Link href="/mypage">마이페이지</Link>
+        <Link href="/mybookingpage">예약현황</Link>
+        <Link href="/mybookinglist">예약내역</Link>
+      </div>
     </>
   );
 }
--- src/pages/mybookinglist.tsx
+++ src/pages/mybookinglist.tsx
@@ -1,19 +1,14 @@
-import List from "@/components/mypage/List";
 import Profile from "@/components/common/profile/Profile";
 
-export default function mypage() {
+export default function mybookinglist() {
   return (
     <>
       <div className="flex justify-center gap-6 bg-gray_FA pt-[72px]">
         <Profile />
         <div className="flex w-[792px] flex-col gap-[24px]">
           <div className="flex justify-between">
-            <h1 className="text-3xl font-bold">내 정보</h1>
-            <button className="h-[48px] w-[120px] rounded bg-nomad_black px-4 py-2 text-white">
-              저장하기
-            </button>
+            <h1 className="text-3xl font-bold">예약 내역</h1>
           </div>
-          <List />
         </div>
       </div>
     </>
