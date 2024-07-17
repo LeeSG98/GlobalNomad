08b9d56a2181d268c989ab3170bb0864044a5872
LeeSoonGu <rntnsl9280@naver.com>
2024-07-17 15:37:02 +0900

    Docs: 경로 수정

--- src/components/signup/LinkToSignin.tsx
+++ src/components/signup/LinkToSignin.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const LinkToSignup = () => (
   <div className="mx-auto my-0 mt-8 flex w-[640px] items-center justify-center gap-2 font-normal">
     <div className="text-[#4B4B4B]">회원이신가요?</div>
-    <Link href="/login" legacyBehavior>
+    <Link href="/signin" legacyBehavior>
       <a className="text-green-80 underline underline-offset-2">로그인하기</a>
     </Link>
   </div>
