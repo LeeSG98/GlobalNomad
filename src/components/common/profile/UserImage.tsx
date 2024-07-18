9350c05a49c2af81ea934583c9e8008b8b177a20
ChoTD <127819128+ChoTD@users.noreply.github.com>
2024-07-18 10:26:01 +0900

    Feat: 프로필 이미지 업로드

--- src/components/common/profile/UserImage.tsx
+++ src/components/common/profile/UserImage.tsx
@@ -5,6 +5,7 @@ import { HiOutlinePencil } from "react-icons/hi2";
 
 interface UserImageProps {
   initialSrc?: string;
+  onImageUpload?: (url: string) => void;
 }
 
 const UserImage: React.FC<UserImageProps> = ({ initialSrc }) => {
@@ -71,56 +72,3 @@ const UserImage: React.FC<UserImageProps> = ({ initialSrc }) => {
 };
 
 export default UserImage;
-
-// import React, { useState, ChangeEvent } from "react";
-// import { FaRegUser } from "react-icons/fa";
-// import { HiOutlinePencil } from "react-icons/hi2";
-// import axiosInstance from "@/lib/axiosinstance";
-
-// interface UserImageProps {
-//   imageSrc?: string;
-// }
-
-// export default function UserImage({ imageSrc }: UserImageProps) {
-//   const [src, setSrc] = useState<string | undefined>(imageSrc);
-//   const [file, setFile] = useState<File | null>(null);
-
-//   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const file = event.target.files?.[0];
-//     if (file) {
-//       setFile(file);
-//       const reader = new FileReader();
-//       reader.onload = () => setSrc(reader.result as string);
-//       reader.readAsDataURL(file);
-//     }
-//   };
-
-//   return (
-//     <div className="relative">
-//       {src ? (
-//         <img
-//           src={src}
-//           alt="유저이미지"
-//           className="h-[160px] w-[160px] rounded-full shadow"
-//         />
-//       ) : (
-//         <div className="flex h-[160px] w-[160px] items-center justify-center rounded-full bg-gray-200 shadow">
-//           <FaRegUser className="text-6xl text-gray-400" />
-//         </div>
-//       )}
-//       <label
-//         htmlFor="imageUpload"
-//         className="absolute bottom-0 right-2 flex h-[44px] w-[44px] cursor-pointer items-center justify-center rounded-full bg-green_0B"
-//       >
-//         <HiOutlinePencil className="text-white" />
-//         <input
-//           id="imageUpload"
-//           type="file"
-//           accept="image/*"
-//           onChange={handleImageChange}
-//           className="hidden"
-//         />
-//       </label>
-//     </div>
-//   );
-// }
